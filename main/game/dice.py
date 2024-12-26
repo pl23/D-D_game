71233894034e04c5facc387a29036f20a9963b3e
pappy <plunkttg@gmail.com>
2024-12-26 13:24:31 -0700

    a

---
+++ main/game/dice.py
@@ -0,0 +1,20 @@
+import random
+
+class dice ():
+    def __init__(self):
+        pass
+    
+    def Role(side):
+        if side != type(int):
+            result = random.randint(1,side)
+            return result 
+        else:
+            print(f"1d{side} is not a valed die")
+    def DiceList(List):
+        for _ in list:
+            
+
+
+
+    def Dice ():
+        pass
---
+++ main/game/main.py
@@ -0,0 +1,23 @@
+import pygame
+
+class game ():
+    def __init__(self):
+
+        pygame.init()
+        screen = pygame.display.set_mode((1280, 720))
+        clock = pygame.time.Clock()
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            screen.fill("purple")
+
+            pygame.display.flip()
+
+            clock.tick(60)  
+
+        pygame.quit()
+game()
