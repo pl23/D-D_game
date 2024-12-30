2d9fa7a69b24f39e2fe9eae9528a53fb8e374e2c
pappy <plunkttg@gmail.com>
2024-12-30 16:19:48 -0700

    update

--- testing/map.py
+++ testing/map.py
@@ -1,29 +1,61 @@
-class player:
-    def __init__(self,vew):
-        self.map = map
+from pprint import pprint
+from copy import deepcopy 
+import json, numpy
+
+path = "map.json"
+class Player:
+    def __init__(self,):
         self.character_positions = [None,None]
+        self.player_direction = None
     def move(self,x,y):
-        self.character_positions += [x,x]
+        self.character_positions += [x,y]
+        out = [self.character_positions]
+        if x > 0:
+            out.append("n")
+        if x [0] > 0:
+            out.append("s")
+        if y [1] < 0:
+            out.append("e")
+        if y [1] > 0:
+            out.append("w")
+        return out
 
 
-class cam:
-     def __init__(self,map):
-        self.vew = [[[]]]
-        self.vew_position = []
-        self.vew_size = [5,5]
-		for x in range(vew_size[0]:):
-            for y in range(vew_size[1]):
-                for z in len map[x][y][z]:
-                    vew.apend(map[x][y][z])
+
+
+class Cam:
     
+    def __init__(self, game_map, ):
+        self.game_map = game_map["game_map"]
+        self.player = Player()
+        self.view_port = [[[]]]
 
-         
+    def create_view_port(self,view_size = [0,0],view_position=[0,0]):
+        self.view_size = view_size
+        self.view_position = view_position
+        for self.view_position[0] in range(self.view_size[0]):
+            for self.view_position[1] in range(self.view_size[1]):
+                for iz in range(self.game_map[0][0]):
+                    self.view_port.append(self.game_map[0][0][0])
+                
+        return self.view_port
 
 
-        
 
-map = {
-    [
+    
+    def move_player (self):
+        pass
+
+    def p(self):
+        pprint(self.view_port)
+
+
+
+
+
+
+
+data = {"game_map":[
         [["grass",True,"player"],["grass",True,],["grass",True,],[None],[None],[None],[None],[None],[None],[None],],
         [["grass",True,],["grass",True,],["grass",True,],[None],[None],[None],[None],[None],[None],[None],],
         [["grass",True,],["grass",True,],["grass",True,],[None],[None],[None],[None],[None],[None],[None],],
@@ -32,6 +64,16 @@ map = {
         [[None],[None],[None],[None],[None],[None],[None],[None],[None],[None],],
         [[None],[None],[None],[None],[None],[None],[None],[None],[None],[None],],
         [[None],[None],[None],[None],[None],[None],[None],[None],[None],[None],],
-        [[None],[None],[None],[None],[None],[None],[None],[None],[None],[None],],
-    ]
+        [[None],[None],[None],[None],[None],[None],[None],[None],[None],[None],],]
 }
+class J:
+    def __init__(self):
+        pass
+    def l(self):
+        with open(path, "r") as f:
+            data = json.load(f)
+        return data
+
+if __name__ == "__main__":
+    c = Cam(data)
+    pprint(c.create_view_port())
